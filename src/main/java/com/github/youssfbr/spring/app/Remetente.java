54156fced988d5672754a3b4367fcdda5a6401fc
Alisson Youssf <youssfbr@gmail.com>
2023-03-26 14:33:50 -0300

    Configuration Properties

---
+++ src/main/java/com/github/youssfbr/spring/app/Remetente.java
@@ -0,0 +1,41 @@
+package com.github.youssfbr.spring.app;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@ConfigurationProperties(prefix = "remetente")
+public class Remetente {
+
+    private String nome;
+    private String email;
+    private List<String> telefones = new ArrayList<>();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<String> getTelefones() {
+        return telefones;
+    }
+
+    public void setTelefones(List<String> telefones) {
+        this.telefones = telefones;
+    }
+
+}
--- src/main/java/com/github/youssfbr/spring/app/SistemaMensagem.java
+++ src/main/java/com/github/youssfbr/spring/app/SistemaMensagem.java
@@ -1,29 +1,21 @@
 package com.github.youssfbr.spring.app;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @Component
 public class SistemaMensagem implements CommandLineRunner {
 
-    @Value("${name:NoReply}")
-    private String nome;
-    @Value("${email}")
-    private String email;
-    @Value("${telefones}")
-    private List<String> telefones = new ArrayList<>();
+    @Autowired
+    private Remetente remetente;
 
     @Override
     public void run(String... args) throws Exception {
         System.out.println("Mensagem enviada por: "
-                + nome
-                + "\nE-mail: " + email
-                + "\nTelefones: " + telefones);
+                + remetente.getNome()
+                + "\nE-mail: " + remetente.getEmail()
+                + "\nTelefones: " + remetente.getTelefones());
 
         System.out.println("Seu cadastro foi aprovado.");
     }
